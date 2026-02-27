2c6aee882d8122672f9e1611ef10218383fab06c
laodeaksar <akhsarode@gmail.com>
2026-02-27 19:14:57 +0800

    first commit

---
+++ services/config-server/config-server/src/test/java/com/aksar/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aksar.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
