e3190fd9e1d90d60c30f029ec9cbefb6f58f899f
waodebole <waodebole@gmail.com>
2026-03-21 10:08:30 +0000

    product

---
+++ services/product/src/main/java/com/aksar/ecommerce/category/Category.java
@@ -0,0 +1,33 @@
+package com.aksar.ecommerce.category;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+import com.aksar.ecommerce.product.Product;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String description;
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
