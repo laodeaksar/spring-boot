1edf3ddb1353d0d7718987615abd6021a583b509
Aksar La'ode <101698503+laodeaksar@users.noreply.github.com>
2026-03-21 09:33:26 +0000

    fix

--- services/customer/src/main/java/com/aksar/ecommerce/exception/CustomerNotFoundException.java
+++ services/customer/src/main/java/com/aksar/ecommerce/exception/CustomerNotFoundException.java
@@ -6,5 +6,5 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class CustomerNotFoundException extends RuntimeException{
-private final String msg;
+private final String message;
 }
