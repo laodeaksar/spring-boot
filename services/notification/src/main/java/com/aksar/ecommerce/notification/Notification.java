b7c309c00f146455d1948d75475726f305a51b46
waodebole <waodebole@gmail.com>
2026-03-31 00:36:04 +0000

    notification service

---
+++ services/notification/src/main/java/com/aksar/ecommerce/notification/Notification.java
@@ -0,0 +1,30 @@
+package com.aksar.ecommerce.notification;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.aksar.ecommerce.kafka.order.OrderConfirmation;
+import com.aksar.ecommerce.kafka.payment.PaymentConfirmation;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Notification {
+
+    @Id
+    private String id;
+    private NotificationType type;
+    private LocalDateTime notificationDate;
+    private OrderConfirmation orderConfirmation;
+    private PaymentConfirmation paymentConfirmation;
+}
---
+++ services/notification/src/main/java/com/aksar/ecommerce/notification/NotificationType.java
@@ -0,0 +1,6 @@
+package com.aksar.ecommerce.notification;
+
+public enum NotificationType {
+    ORDER_CONFIRMATION,
+    PAYMENT_CONFIRMATION
+}
